3e86a01ade123497c204a23c7e97262b8e6f430f
Etienne Selemani <etienneselemani47@gmail.com>
2023-06-30 23:25:34 +0200

    add comments

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import pyttsx3
 engine = pyttsx3.init()
 
 """VOLUME"""
-volume = engine.getProperty('volume')
-engine.setProperty('volume', 1.0)
+volume = engine.getProperty('volume') #getting to know current volume level (min=0 and max=1)
+engine.setProperty('volume', 1.0)# setting up volume level  between 0 and 1
 
 """RATE"""
-rate = engine.getProperty('rate')
-engine.setProperty('rate', 125)
+rate = engine.getProperty('rate') # getting details of current speaking rate
+engine.setProperty('rate', 125)  # setting up new voice rate
 
 
 """VOICE"""
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+voices = engine.getProperty('voices') #getting details of current voice
+engine.setProperty('voice', voices[1].id) #changing index, changes voices. 1 for female
 
 names = ['etienne selemani', 'junior shindano', 'franck tshiela', 'roger', 'enoch', 'mardochée']
 def reader_name(nameList):
